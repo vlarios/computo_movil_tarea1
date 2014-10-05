e7a8fd998bf4f6b58240ec5fd519f13b7cf65c88
Victor Larios <victorlarios@Victors-Mac-Pro.local>
2014-10-05 13:59:59 -0700

    Tarea1, segunda version

--- Tarea1/ViewController.h
+++ Tarea1/ViewController.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *colorButton;
+@property (weak, nonatomic) IBOutlet UIButton *backgroundButton;
+@property (weak, nonatomic) IBOutlet UIImageView *myImage;
+@property (weak, nonatomic) IBOutlet UIButton *changeImageButton;
+@property (weak, nonatomic) IBOutlet UILabel *myName;
+@property (strong, nonatomic) IBOutlet UIView *view1;
+@property (weak, nonatomic) IBOutlet UIButton *sayHelloButon;
+@property (weak, nonatomic) IBOutlet UILabel *screen2Label;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UIImageView *cityPhoto;
+@property (weak, nonatomic) IBOutlet UIImageView *clubPhoto;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *clubSelector;
+@property (weak, nonatomic) IBOutlet UIButton *incGol;
+@property (weak, nonatomic) IBOutlet UIButton *decGol;
+@property (weak, nonatomic) IBOutlet UILabel *score;
+@property (weak, nonatomic) IBOutlet UIButton *playMovei;
+
+
 
 @end
--- Tarea1/ViewController.m
+++ Tarea1/ViewController.m
@@ -14,6 +14,10 @@
 
 @implementation ViewController
 
+int cnt=0;
+int colorCnt=0;
+int backgroundCnt=0;
+int scoreCnt=0;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -24,6 +28,113 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)changeColor:(id)sender {
+    if (colorCnt==0) {
+        self.myName.textColor = [UIColor redColor];
+        colorCnt++;
+    }
+    else if (colorCnt==1) {
+        self.myName.textColor = [UIColor greenColor];
+        colorCnt++;
+    }
+    else if (colorCnt==2) {
+        self.myName.textColor = [UIColor blueColor];
+        colorCnt++;
+    }
+    else if (colorCnt==3) {
+        self.myName.textColor = [UIColor orangeColor];
+        colorCnt++;
+    }
+    else if (colorCnt==4) {
+        self.myName.textColor = [UIColor blackColor];
+        colorCnt=0;
+    }
+}
+
+- (IBAction)changeBackground:(id)sender {
+    if (backgroundCnt==0) {
+        self.view1.backgroundColor = [UIColor yellowColor];
+        backgroundCnt++;
+    }
+    else if (backgroundCnt==1) {
+        self.view1.backgroundColor = [UIColor redColor];
+        backgroundCnt++;
+    }
+    else if (backgroundCnt==2) {
+        self.view1.backgroundColor = [UIColor greenColor];
+        backgroundCnt++;
+    }
+    else if (backgroundCnt==3) {
+        self.view1.backgroundColor = [UIColor blueColor];
+        backgroundCnt++;
+    }
+    else if (backgroundCnt==4) {
+        self.view1.backgroundColor = [UIColor whiteColor];
+        backgroundCnt=0;
+    }
+    
+}
+- (IBAction)sayHello:(id)sender {
+    self.screen2Label.text = @"Hello";
+}
+
+- (IBAction)sliderCtl:(id)sender {
+    self.cityPhoto.alpha = self.slider.value;
+}
+
+- (IBAction)changeImage:(id)sender {
+    
+    
+    
+    if (cnt==0) {
+        UIImage *image = [UIImage imageNamed: @"photo2.jpg"];
+        [self.myImage setImage:image];
+        cnt++;
+    }
+    else if (cnt==1) {
+        UIImage *image = [UIImage imageNamed: @"photo3.jpg"];
+        [self.myImage setImage:image];
+        cnt++;
+    }
+    else if (cnt==2) {
+        UIImage *image = [UIImage imageNamed: @"photo1.jpg"];
+        [self.myImage setImage:image];
+        cnt++;
+        cnt=0;
+    }
+    
+}
+- (IBAction)incScoreCtl:(id)sender {
+    scoreCnt++;
+    self.score.text=[NSString stringWithFormat:@"%i",scoreCnt];
+}
+
+- (IBAction)decScoreCtl:(id)sender {
+    scoreCnt--;
+    self.score.text=[NSString stringWithFormat:@"%i" ,scoreCnt];
+}
+
+
+- (IBAction)clubSelectControl:(id)sender {
+    
+    if (self.clubSelector.selectedSegmentIndex == 0) {
+        UIImage *image = [UIImage imageNamed: @"chivas-guadalajara-211.jpg"];
+        [self.clubPhoto setImage:image];
+    }
+    else if (self.clubSelector.selectedSegmentIndex == 1) {
+        UIImage *image = [UIImage imageNamed:@"america.jpg"];
+        [self.clubPhoto setImage:image];
+    }}
+
+- (IBAction)playMovieCtl:(id)sender{
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"dogs" ofType: @"mp4"] ];
+    MPMoviePlayerViewController *playercontroller = [[MPMoviePlayerViewController alloc] initWithContentURL:url];
+    [self presentMoviePlayerViewControllerAnimated: playercontroller];
+    playercontroller.moviePlayer.movieSourceType = MPMovieSourceTypeFile;
+    [playercontroller.moviePlayer play];
+    playercontroller = nil;
 }
 
 @end
